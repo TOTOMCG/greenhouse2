bbf833c9a42c23771f7d20602ffd926dc2a3d94f
nikitakitsune <insta.nekitus@gmail.com>
2023-02-16 18:44:11 +0300

    v6

--- greenhouse/greenhouse/urls.py
+++ greenhouse/greenhouse/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/main', permanent=True)),
     path('main/', include('main.urls')),
 ]
 
--- greenhouse/main/urls.py
+++ greenhouse/main/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
-from django.views.generic import RedirectView
 from . import views
-from django.conf import settings
 from django.conf.urls.static import static 
 
 # влажность 
