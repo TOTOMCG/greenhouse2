d9ceeba22a68755c61bcad0240c7c44eea6217e9
nikitakitsune <insta.nekitus@gmail.com>
2023-02-17 11:42:18 +0300

    v6 alfa-build

--- greenhouse/records_manager/views.py
+++ greenhouse/records_manager/views.py
@@ -1,1 +1,8 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.views.generic.list import ListView
+
+#обработка модели средних значений для вывода в шаблон
+class ModelName(ListView):
+    model = ModelName
+    template_name = 'main.html'
+    context_object_name = 'average'
