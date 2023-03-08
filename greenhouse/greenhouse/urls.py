15e9659622f5a96f226162744f5ba7cab7d2fd9b
nikitakitsune <insta.nekitus@gmail.com>
2023-03-08 21:23:45 +0300

    v9 build "true" 2

--- greenhouse/greenhouse/urls.py
+++ greenhouse/greenhouse/urls.py
@@ -7,22 +7,22 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', RedirectView.as_view(url='/main', permanent=True)),
     path('main/', include('main.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+"""
+urlpatterns += [
+  path('jsons/', include('jsons.urls') )
 ]
 
-# urlpatterns += [
-#     path('jsons/', include('jsons.urls') )
-# ]
-#
-# setting
-# urlpatterns += [
-#     path('settings/', include('settings.urls')),
-#     path('config/', RedirectView.as_view(url='/settings/', permanent=True)),
-#     path('setting/', RedirectView.as_view(url='/settings/', permanent=True))
-# ]
-#
-# # database
-# urlpatterns += [
-#     path('database/', include('database.urls')),
-#     path('databases/', RedirectView.as_view(url='/database/', permanent=True)),
-#     path('db/', RedirectView.as_view(url='/database/', permanent=True))
-# ]
+urlpatterns += [
+  path('settings/', include('settings.urls')),
+  path('config/', RedirectView.as_view(url='/settings/', permanent=True)),
+  path('setting/', RedirectView.as_view(url='/settings/', permanent=True))
+]
+
+urlpatterns += [
+  path('database/', include('database.urls')),
+  path('databases/', RedirectView.as_view(url='/database/', permanent=True)),
+  path('db/', RedirectView.as_view(url='/database/', permanent=True))
+ ]
+"""
